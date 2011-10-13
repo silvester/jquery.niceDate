c9b5cbf7698d1aa4a29dad2979093f87d13677a1
Silvester Maraz <silvester.maraz@reverse.si>
2011-10-13 14:41:32 +0200

    Made more configurable

---
+++ i18n/sl.js
@@ -0,0 +1,52 @@
+/*Slovenian date time formats*/
+$.extend($.fn.niceDate.defaults, {
+
+	pattern : /([0-3][0-9]).([0|1][0-9]).(\d{4})\s(\d{2}):(\d{2})$/, // 15.08.2011 15:30
+
+	objTs : [3, 2, 1, 4, 5], // year, month, day, hour, minute
+
+	dayMessages : {
+		n : {
+			0 : 'Danes',
+			1 : 'Včeraj',
+			2 : 'Pred 2 dnevoma',
+			many : 'Pred %s dnevi'
+		},
+		p : {
+			0 : 'Danes',
+			1 : 'Jutri',
+			many : 'Čez %s dni'
+		}
+	},
+
+	hourMessages : {
+		n : {
+			1 : 'Pred 1 uro',
+			2 : 'Pred 2 urama',
+			many : 'Pred %s urami'
+		},
+		p : {
+			1 : 'Čez 1 uro',
+			2 : 'Čez 2 uri',
+			3 : 'Čez 3 ure',
+			4 : 'Čez 4 ure',
+			many : 'Čez %s ur'
+		}
+	},
+
+	minMessages : {
+		n : {
+			1 : 'Pred 1 minuto',
+			2 : 'Pred 2 minutoma',
+			many : 'Pred %s minutami'
+		},
+		p : {
+			1 : 'Čez 1 minuto',
+			2 : 'Čez 2 minuti',
+			3 : 'Čez 3 minute',
+			4 : 'Čez 4 minute',
+			many : 'Čez %s minut'
+		}
+	}
+
+});
--- jquery.niceDate.js
+++ jquery.niceDate.js
@@ -90,16 +90,36 @@
 		nowTs:	 		Math.round(new Date().getTime() / 1000),
 		pattern: 		/([0-3][0-9]).([0|1][0-9]).(\d{4})\s(\d{2}):(\d{2})$/, // 15.08.2011 15:30
 		patternOrder: 	[3, 2, 1, 4, 5], // year, month, day, hour, minute
+		dayMessages : {
+			n : {0 : 'Today', 1 : 'Yesterday', many : '%s days before'},
+			p : {0 : 'Today', 1 : 'Tomorrow', many : '%s days after'}
+		},
+		hourMessages : {
+			n : {1 : '%s hour before', many : '%s hours before'},
+			p : {1 : 'After %s hour', many : 'After %s hours'}
+		},
+		minMessages : {
+			n : {1 : '% minute before', many : '%s minutes before'},
+			p : {1 : 'After %s minute',	many : 'After %s minutes'}
+		}
 	};
 	
 	$.fn.niceDate.defaults.makeTimestamp = function(text)
 	{
+		
+		var o = $.fn.niceDate.defaults;
 		var matches = text.match(o.pattern);
+		
 		if(matches) {
-			return new Date(matches[o.patternOrder[0]], matches[o.patternOrder[1]] - 1, matches[o.patternOrder[2]], matches[o.patternOrder[3]], matches[o.patternOrder[4]]);
+			return new Date(matches[o.patternOrder[0]], 
+							matches[o.patternOrder[1]] - 1, 
+							matches[o.patternOrder[2]], 
+							matches[o.patternOrder[3]], 
+							matches[o.patternOrder[4]]);
 		} else {
-			return;
+			return false;
 		}
+		
 	}
 
 })(jQuery);
